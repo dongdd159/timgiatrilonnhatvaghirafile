705ece02f314d1c7909fd3b144ee95f5e477402f
dongdd159 <79952800+dongdd159@users.noreply.github.com>
2021-04-26 14:00:45 +0700

    timgiatrilonnhatvaghirafile

---
+++ src/FindMaxValue.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class FindMaxValue {
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (int i = 0; i < numbers.size(); i++) {
+            if (max < numbers.get(i)) {
+                max = numbers.get(i);
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        List<Integer> numbers = readAndWriteFile.readFile("C:\\Users\\admin\\OneDrive\\Máy tính\\file.txt");
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile("C:\\Users\\admin\\OneDrive\\Máy tính\\result.txt", maxValue);
+    }
+}
